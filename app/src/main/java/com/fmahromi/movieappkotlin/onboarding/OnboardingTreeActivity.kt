ef267896970c26acfd16a6636011060a096c478a
Faisal <fmahromi@gmail.com>
2022-05-07 16:28:03 +0700

    initialization project

---
+++ app/src/main/java/com/fmahromi/movieappkotlin/onboarding/OnboardingTreeActivity.kt
@@ -0,0 +1,12 @@
+package com.fmahromi.movieappkotlin.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fmahromi.movieappkotlin.R
+
+class OnboardingTreeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding_tree)
+    }
+}
